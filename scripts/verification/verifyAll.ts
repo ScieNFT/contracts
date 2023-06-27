84d6f9c75fa1c907de609d5a1d02bc5a15c7da5e
Robb Walters <robb@scienft.com>
2023-06-27 23:45:38 +0200

    construct parameters as in deployment scripts

--- scripts/verification/verifyAll.ts
+++ scripts/verification/verifyAll.ts
@@ -5,34 +5,58 @@ import * as fs from 'fs';
 import { run } from 'hardhat';
 import dotenv from 'dotenv';
 import hre from 'hardhat';
+import { BigNumber } from 'ethers';
 
 dotenv.config({ path: './.env' });
 
 const deploymentFilePath = join(__dirname, '../../deployment.config.43113.json');
 const deploymentJson = JSON.parse(fs.readFileSync(deploymentFilePath, 'utf8'));
 
+// from deployment scripts
+let uri: string | undefined = process.env.METADATA_JSON_URI;
+const CONTRACT_DEPLOYMENT_URI: string = uri ? uri : 'http://www.scienft.com/token-{id}.json';
+
+let envInitialMiningYield: string | undefined = process.env.INITIAL_MINING_YIELD_SCI;
+let initialMiningYield = BigNumber.from(envInitialMiningYield).mul(BigNumber.from(10).pow(18));
+
+let envMinimumMiningYield: string | undefined = process.env.MINIMUM_MINING_YIELD_SCI;
+let minimumMiningYield = BigNumber.from(envMinimumMiningYield).mul(BigNumber.from(10).pow(18));
+
+let envMiningFee: string | undefined = process.env.MINING_FEE_GAS;
+let miningFee = BigNumber.from(envMiningFee);
+
+let envDifficulty: string | undefined = process.env.DIFFICULTY;
+let difficulty = BigNumber.from(envDifficulty);
+
+let envMiningIntervalSeconds: string | undefined = process.env.MINING_INTERVAL_SECONDS;
+let miningIntervalSeconds = BigNumber.from(envMiningIntervalSeconds);
+
+let envMaxTotalSupply: string | undefined = process.env.MAXIMUM_TOTAL_SUPPLY_SCI;
+let maxTotalSupply = BigNumber.from(envMaxTotalSupply).mul(BigNumber.from(10).pow(18));
+
+let envMintingFee: string | undefined = process.env.MINTING_FEE_GAS;
+let mintingFee = BigNumber.from(envMintingFee);
+
+let envListingFee: string | undefined = process.env.DEFAULT_LISTING_FEE_GAS;
+const listingFee: number = envListingFee ? parseInt(envListingFee) : 0;
+
+let envRoyaltyNumerator: string | undefined = process.env.DEFAULT_ROYALTY_NUMERATOR;
+const royaltyNumerator: number = envRoyaltyNumerator ? parseInt(envRoyaltyNumerator) : 0;
+
 // Map of contract names to constructor values
-const constructorValuesMap: { [key: string]: (string | number)[] } = {
+const constructorValuesMap: { [key: string]: any[] } = {
   Tokens: [
-    process.env.METADATA_JSON_URI!, // uri_
-    process.env.INITIAL_MINING_YIELD_SCI!, // initialMiningYield
-    process.env.MINIMUM_MINING_YIELD_SCI!, // minimumMiningYield_
-    process.env.MINING_FEE_GAS!, // miningFee_
-    process.env.DIFFICULTY!, // difficulty_
-    process.env.MINING_INTERVAL_SECONDS!, // miningIntervalSeconds_
-    process.env.MAXIMUM_TOTAL_SUPPLY_SCI!, // maxTotalSupply_
-    process.env.MINTING_FEE_GAS!, // mintingFee_
+    CONTRACT_DEPLOYMENT_URI,
+    initialMiningYield,
+    minimumMiningYield,
+    miningFee,
+    difficulty,
+    miningIntervalSeconds,
+    maxTotalSupply,
+    mintingFee,
   ],
-  Offers: [
-    deploymentJson.tokensAddress, // tokensContractAddress
-    process.env.DEFAULT_LISTING_FEE_GAS!, // offerFee_
-    process.env.DEFAULT_ROYALTY_NUMERATOR!, // royaltyNumerator_
-  ],
-  Listings: [
-    deploymentJson.tokensAddress, // tokensContractAddress
-    process.env.DEFAULT_LISTING_FEE_GAS!, // listingFee_
-    process.env.DEFAULT_ROYALTY_NUMERATOR!, // royaltyNumerator_
-  ],
+  Offers: [deploymentJson.tokensAddress, listingFee, royaltyNumerator],
+  Listings: [deploymentJson.tokensAddress, listingFee, royaltyNumerator],
 };
 
 // Map of contract names to contract addresses
@@ -51,7 +75,8 @@ async function main() {
 
   for (let contractName of contractNames) {
     console.log(`${contractName}.sol was deployed to ${addressMap[contractName]}`);
-    // console.log(constructorValuesMap[contractName]);
+    console.log(constructorValuesMap[contractName]);
+
     await run('verify:verify', {
       address: addressMap[contractName],
       constructorArguments: constructorValuesMap[contractName],
