4652e4df638d4a8ce07c12e78d7754bef2541f50
Robb Walters <robb@scienft.com>
2023-06-27 23:54:42 +0200

    fix getArguments script

--- scripts/verification/getArguments.js
+++ scripts/verification/getArguments.js
@@ -1,6 +1,7 @@
 const ethers = require('ethers');
 const fs = require('fs');
 const path = require('path');
+const { BigNumber } = require('ethers');
 
 const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
@@ -10,28 +11,51 @@ const deploymentJson = JSON.parse(fs.readFileSync(deploymentFilePath, 'utf8'));
 
 console.log(`The tokens contract was deployed to ${deploymentJson.tokensAddress}`);
 
+// from deployment scripts
+let uri = process.env.METADATA_JSON_URI;
+const CONTRACT_DEPLOYMENT_URI = uri ? uri : 'http://www.scienft.com/token-{id}.json';
+
+let envInitialMiningYield = process.env.INITIAL_MINING_YIELD_SCI;
+let initialMiningYield = BigNumber.from(envInitialMiningYield).mul(BigNumber.from(10).pow(18));
+
+let envMinimumMiningYield = process.env.MINIMUM_MINING_YIELD_SCI;
+let minimumMiningYield = BigNumber.from(envMinimumMiningYield).mul(BigNumber.from(10).pow(18));
+
+let envMiningFee = process.env.MINING_FEE_GAS;
+let miningFee = BigNumber.from(envMiningFee);
+
+let envDifficulty = process.env.DIFFICULTY;
+let difficulty = BigNumber.from(envDifficulty);
+
+let envMiningIntervalSeconds = process.env.MINING_INTERVAL_SECONDS;
+let miningIntervalSeconds = BigNumber.from(envMiningIntervalSeconds);
+
+let envMaxTotalSupply = process.env.MAXIMUM_TOTAL_SUPPLY_SCI;
+let maxTotalSupply = BigNumber.from(envMaxTotalSupply).mul(BigNumber.from(10).pow(18));
+
+let envMintingFee = process.env.MINTING_FEE_GAS;
+let mintingFee = BigNumber.from(envMintingFee);
+
+let envListingFee = process.env.DEFAULT_LISTING_FEE_GAS;
+const listingFee = envListingFee ? parseInt(envListingFee) : 0;
+
+let envRoyaltyNumerator = process.env.DEFAULT_ROYALTY_NUMERATOR;
+const royaltyNumerator = envRoyaltyNumerator ? parseInt(envRoyaltyNumerator) : 0;
+
 // Map of contract names to constructor values
 const constructorValuesMap = {
   Tokens: [
-    process.env.METADATA_JSON_URI, // uri_
-    process.env.INITIAL_MINING_YIELD_SCI, // initialMiningYield
-    process.env.MINIMUM_MINING_YIELD_SCI, // minimumMiningYield_
-    process.env.MINING_FEE_GAS, // miningFee_
-    process.env.DIFFICULTY, // difficulty_
-    process.env.MINING_INTERVAL_SECONDS, // miningIntervalSeconds_
-    process.env.MAXIMUM_TOTAL_SUPPLY_SCI, // maxTotalSupply_
-    process.env.MINTING_FEE_GAS, // mintingFee_
+    CONTRACT_DEPLOYMENT_URI,
+    initialMiningYield,
+    minimumMiningYield,
+    miningFee,
+    difficulty,
+    miningIntervalSeconds,
+    maxTotalSupply,
+    mintingFee,
   ],
-  Offers: [
-    deploymentJson.tokensAddress, // tokensContractAddress
-    process.env.DEFAULT_LISTING_FEE_GAS, // offerFee_
-    process.env.DEFAULT_ROYALTY_NUMERATOR, // royaltyNumerator_
-  ],
-  Listings: [
-    deploymentJson.tokensAddress, // tokensContractAddress
-    process.env.DEFAULT_LISTING_FEE_GAS, // listingFee_
-    process.env.DEFAULT_ROYALTY_NUMERATOR, // royaltyNumerator_
-  ],
+  Offers: [deploymentJson.tokensAddress, listingFee, royaltyNumerator],
+  Listings: [deploymentJson.tokensAddress, listingFee, royaltyNumerator],
 };
 
 // Define the contract names
